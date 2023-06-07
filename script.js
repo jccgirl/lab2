aabed8d6fbc5713abb9097f27453b423b164833a
Jasmin Saleh <rosejasmin@hotmail.de>
2023-06-07 13:33:47 +0200

    changed structure, limiter is not working

--- script.js
+++ script.js
@@ -1,50 +1,58 @@
-window.onload = function() {
-    var sinkship = {
-      makeLimiter: function() {
-        var limiter = document.createElement("div");
-        limiter.className = "limiter";
-        return limiter;
-      },
-  
-      createHeader: function() {
-        var header = document.createElement("header");
-        var limiter = this.makeLimiter();
-        var heading = document.createElement("h1");
-        heading.textContent = "Sink Ship";
-        var paragraph = document.createElement("p");
-        paragraph.textContent = "by Jasmin Saleh";
-  
-        header.appendChild(heading);
-        header.appendChild(paragraph);
-        document.body.appendChild(header);
-        header.appendChild(limiter);
-      },
-  
-      createMain: function() {
-        var main = document.createElement("main");
-        document.body.appendChild(main);
-        var limiter = this.makeLimiter();
-        main.appendChild(limiter);
-      },
-  
-      createFooter: function() {
-        var footer = document.createElement("footer");
-        var limiter = this.makeLimiter();
-        var paragraph = document.createElement("p");
-        paragraph.textContent = "©Jasmin Saleh";
-  
-        limiter.appendChild(paragraph);
-        footer.appendChild(limiter);
-        document.body.appendChild(footer);
-      },
-    };
-  
-    window.addEventListener("sinkshipLoaded", function() {
-      sinkship.createHeader();
-      sinkship.createMain();
-      sinkship.createFooter();
-    });
-  
-    window.dispatchEvent(new Event("sinkshipLoaded"));
-  };
-  
+window.addEventListener("load", () => sinkship.init());
+
+const sinkship = {
+  init() {
+    const body = document.body;
+    const content = document.createElement("div");
+    content.classList.add("content");
+    body.appendChild(content);
+    const header = this.makeHeader();
+    content.appendChild(header);
+    const main = this.makeMain();
+    content.appendChild(main);
+    const footer = this.makeFooter();
+    content.appendChild(footer);
+  },
+
+  makeHeader() {
+    const header = document.createElement("header");
+    const limiter = this.makeLimiter();
+    const heading = document.createElement("h1");
+    heading.textContent = "Sink Ship";
+    const paragraph = document.createElement("p");
+    paragraph.textContent = "by Jasmin Saleh";
+
+    header.appendChild(heading);
+    header.appendChild(paragraph);
+    header.appendChild(limiter);
+
+    return header;
+  },
+
+  makeMain() {
+    const main = document.createElement("main");
+    const limiter = this.makeLimiter();
+    main.appendChild(limiter);
+
+    return main;
+  },
+
+  makeFooter() {
+    const footer = document.createElement("footer");
+    const limiter = this.makeLimiter();
+    const paragraph = document.createElement("p");
+    paragraph.textContent = "©Jasmin Saleh";
+
+    limiter.appendChild(paragraph);
+    footer.appendChild(limiter);
+
+    return footer;
+  },
+
+  makeLimiter() {
+    const limiter = document.createElement("div");
+    limiter.className = "limiter";
+    return limiter;
+  },
+
+};
