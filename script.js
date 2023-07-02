ba49a59231267c6efa1610ca235eb5ece973c6c1
Jasmin Saleh <rosejasmin@hotmail.de>
2023-07-02 20:45:06 +0200

    added functionalities

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-window.addEventListener("load", () => sinkship.init());
-
-const sinkship = {
+window.addEventListener("load", () => sinkship.init()); 
+const sinkship = { 
   playerfield:[],
-  computerfield:[],
-
+  computerfield:[], 
+  placeAblePos : new Array(100).fill(-1),  
+  placeAblePosCom:new Array(100).fill(-1),
+  ships_size:[],
+  ships_orien:[],  
+  GameStart: false,
+  GameFinshed: false, 
+  ComputerWater: false, 
+  turn: 1,
+  winer: "",
+  shipsNum: [1,2,3,4],  
+  shipsPlaced:0,
+  
   init() {
+    this.ships_orien[0] = "h"
+    this.ships_size[0] = 3 
     const body = document.body;
     const content = document.createElement("div");
     content.classList.add("content");
@@ -14,7 +26,7 @@ const sinkship = {
     const main = this.makeMain();
     content.appendChild(main);
     const footer = this.makeFooter();
-    content.appendChild(footer);
+    content.appendChild(footer);    
   },
 
   makeHeader() {
@@ -46,8 +58,7 @@ const sinkship = {
     const controlsDiv = this.makeControls();
 
     this.createCells(this.playerfield);
-    //this.createCells(computerfield);
-
+    
     fieldsDiv.appendChild(this.playerfield);
     fieldsDiv.appendChild(computerfield);
     
@@ -63,7 +74,7 @@ const sinkship = {
     const footer = document.createElement("footer");
     const limiter = this.makeLimiter();
     const paragraph = document.createElement("p");
-    paragraph.textContent = "©Jasmin Saleh";
+    paragraph.textContent = "© Jasmin Saleh";
 
     limiter.appendChild(paragraph);
     footer.appendChild(limiter);
@@ -85,32 +96,201 @@ const sinkship = {
 
   createCells(playerfield) {
     const cells = [];
-
     for (let x = 0; x < 10; x++) {
-    const row = [];
+      const row = [];
       for (let y = 0; y < 10; y++) {
-      const cell = document.createElement("div");
-      cell.classList.add("cell");
-      playerfield.appendChild(cell);
-      row[y] = cell;
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        playerfield.appendChild(cell);  
+        const clickHandler = () => {         
+          const Cells = document.querySelectorAll("#playerfield .cell");
+          let at = x * 10 + y;           
+          if(this.removeShip(at,Cells)) {
+            console.log("removed at: "+at);
+          }
+          else{
+            this.shipPlacer(at, Cells); 
+          }
+          if(this.GameStart){ 
+            this.startPlaying(at); 
+          }                                
+        }; 
+        cell.clickHandler = clickHandler;  
+        cell.addEventListener("click", clickHandler);
+        row[y] = cell;
+      }
+      cells[x] = row;
     }
-    cells[x] = row;
+    return cells;
+  },
+
+removeShip(at,Cell){
+  for(let i = 0; i < this.shipCoordinate.length; i++){
+    for(let j = 0; j < this.shipCoordinate[i].length; j++)
+    if(this.shipCoordinate[i][j] == at){
+      this.updateShipNum(this.shipCoordinate[i].length);
+      for(let j = 0; j < this.shipCoordinate[i].length; j++){ 
+         this.updateCell(Cell[this.shipCoordinate[i][j]], this.shipCoordinate[i][j]); 
+         const dx = Math.floor(this.shipCoordinate[i][j] / 10); 
+         const dy = this.shipCoordinate[i][j] % 10; 
+         for(let x = dx-1; x <= dx + 1; x++){
+          for(let y = dy-1; y <= dy + 1; y++){
+            if(x >= 0 && x < 10 && y >= 0 && y < 10 ){ 
+                this.updateCell(Cell[(x*10)+y],(x*10)+y); 
+            }
+          }
+         }
+      }
+      this.shipCoordinate.pop(i);
+      return true;
+    }
   }
-  return cells;
+  return false;
+},
+updateShipNum(size){
+  switch(size){
+    case 5: this.shipsNum[0]++; break;
+    case 4: this.shipsNum[1]++; break;
+    case 3: this.shipsNum[2]++; break;
+    case 2: this.shipsNum[3]++; break;
+  }
+  this.update();
+},
+  
+checker(ships) { 
+  if(ships == 5) { if(this.shipsNum[0] > 0) {this.shipsNum[0]--; return true;} else return false; }
+  if(ships == 4) { if(this.shipsNum[1] > 0) {this.shipsNum[1]--; return true;} else return false;}
+  if(ships == 3) { if(this.shipsNum[2] > 0) {this.shipsNum[2]--; return true;} else return false;}
+  if(ships == 2) { if(this.shipsNum[3] > 0) {this.shipsNum[3]--; return true;} else return false;}
+  return false;
+},
+shipCoordinate:[], 
+shipPlacer(at, field){
+  this.shipsPlaced++;
+  if(this.shipsPlaced === 10) playButton.disabled = false;
+    const Cells = field;
+    var xy = [];
+    this.showMessage(this.ships_size[0],"blue");   
+    if(this.placeAblePos[at] === 0 && this.checker(this.ships_size[0])){ 
+      if(this.ships_orien[0] === "h"){
+        for(let i = at, j = 0 ;  j < this.ships_size[0]; i++, j++){ 
+            if( i === at ) Cells[i].classList.add("leftShip");
+            if( j === this.ships_size[0]-1) Cells[i].classList.add("rightShip");
+            else Cells[i].classList.add("ship");
+            xy.push(i);
+            this.placeAblePos[i] = this.ships_size[0]; 
+        }
+      }
+      else{
+        for(let i = at, j = 0; j < this.ships_size[0]; i = i + 10, j++){ 
+          if( i === at ) Cells[i].classList.add("topShip");
+          if( j === this.ships_size[0]-1) Cells[i].classList.add("bottomShip");
+          else Cells[i].classList.add("ship");
+          xy.push(i);
+            this.placeAblePos[i] = this.ships_size[0];  
+        }
+      }
+      this.shipSecure(Cells);
+      this.update();
+      this.shipCoordinate.push(xy);
+      return true;
+    }
+    return false
+}, 
+shipSecure(cell) { 
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      var P = this.placeAblePos[(i * 10) + j];
+      fx = P;
+      if ( P > 1 && P < 6) {  
+        for (let x = i - 1; x <= i + 1; x++) {          
+          if (x >= 0 && x < 10) {
+            for (let y = j - 1; y <= j + 1; y++) {
+              if (y >= 0 && y < 10) {
+                var p = this.placeAblePos[(x*10)+y] ;
+                if(p !== 2 && p !== 3 && p !== 4 && p !== 5){  
+                  this.placeAblePos[(x * 10) + y] = 6; 
+                  cell[(x*10)+y].classList.add("usable");  
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }  
+   
 },
 
-  makeControls() {
-    const div = this.makeDiv("controls");
-    const buildButton = document.createElement("button");
-    buildButton.textContent = "Build";
-    const playButton = document.createElement("button");
-    playButton.textContent = "Play";
 
-    div.appendChild(buildButton);
-    div.appendChild(playButton);
-    
-    return div;
+showMessage(txt, textColor) {
+  if (textSpan) {
+    textSpan.textContent = txt; 
+    textSpan.classList.add("text-span");
+    textSpan.style.color = textColor; }
 },
+
+makeControls() {
+  const div = this.makeDiv("controls");
+  const buildButton = document.createElement("button");
+  buildButton.textContent = "Build";
+  buildButton.id = "build";
+  playButton = document.createElement("button");
+  playButton.textContent = "Play";
+  const autoButton = document.createElement("button");
+  autoButton.textContent = "Auto Place";
+  textSpan = document.createElement("span");
+  textSpan.textContent = "Your text goes here";
+  textSpan.style.backgroundColor = "white";
+  textSpan.style.display = "none" 
+  playButton.disabled = true;
+
+  div.appendChild(buildButton);
+  div.appendChild(playButton);
+  div.appendChild(autoButton);
+  div.appendChild(textSpan); 
+  buildButton.addEventListener("click", () => {
+    location.reload();
+  });
+  
+  playButton.addEventListener("click", () => {  
+    this.showMessage("Let Play the Game ", "brown");          
+    this.readyToplay();
+    this.GameStart = true;
+    const oldcells = document.querySelectorAll("#playerfield .cell");
+    this.updateClickablity(oldcells);  
+    const computerField = document.getElementById("computerfield");
+    if (computerField) {
+      while (computerField.firstChild) {
+        computerField.firstChild.remove();
+      }
+    }    
+    div.removeChild(buildButton);
+    div.removeChild(playButton);
+    div.removeChild(autoButton); 
+    textSpan.style.display = "inline-block"; 
+    this.createCells(computerfield);  
+ 
+  });
+  autoButton.addEventListener("click", () => { 
+    const Cells = document.querySelectorAll("#playerfield .cell"); 
+    for(let i = 0; i < 100; i++){
+      this.placeAblePos[i] = -1; 
+      Cells[i].classList.remove("usable"); 
+      Cells[i].classList.remove("ship");
+      Cells[i].classList.remove("water");  
+      Cells[i].classList.remove("topShip");
+      Cells[i].classList.remove("bottomShip");
+      Cells[i].classList.remove("leftShip");
+      Cells[i].classList.remove("rightShip");       
+    } 
+    this.update();
+    this.autoPlacing(Cells); 
+    autoButton.disabled = true; // Make the button unclickable
+    playButton.disabled = false;
+  });
+  return div;
+}, 
 
   makeTableHeader(textContent) {
   const tableHeader = document.createElement("th");
@@ -154,33 +334,287 @@ const sinkship = {
     return div;
 
   },
-
+Ships:[],
+Ships_Size:[],
+Ships_Ori:[],
   createTableRow(cellContents, shipType) {
-    const row = document.createElement("tr");
-    
+    const row = document.createElement("tr"); 
     cellContents.forEach((content, index) => {
       const cell = document.createElement("td");
       cell.textContent = content;
       if (index === 1 || index === 2) {
         cell.classList.add(shipType, index === 1 ? "hori" : "verti");
-    //Adding the logic for the Eventhandler when clicking on the images in the table 
-        cell.addEventListener("click", () => {
+        // Adding the logic for the Eventhandler when clicking on the images in the table  
+        cell.addEventListener("click", () => { 
           const typ = row.childNodes[3].textContent;
-          const grösse = row.childNodes[4].textContent; 
+          const size = row.childNodes[4].textContent;
           const orientation = event.currentTarget.classList.contains("hori") ? "h" : "v";
-          console.log(`Typ: ${typ}, Grösse: ${grösse}, Orientation: ${orientation}`);
-        });
-      }
-      row.appendChild(cell);
-    });
-    
+          console.log(`Typ: ${typ}, Grösse: ${size}, Orientation: ${orientation}`);    
+          this.Ships_Ori.push(orientation);
+          if(this.shipsNum[5-size] !== 0)
+            this.showPositions(size, orientation); 
+      });       
+    this.Ships_Size = [5,5,4,4,3,3,2,2];
+    this.Ships.push(cell);
+    }  
+    row.appendChild(cell);
+  });
     return row;
-  },
+  },  
 
-  showPositions(Grösse, shipType) {
-    this.playerfield[0][0].style.backgroundColor = "green";
-    
+  
+  
+  showPositions(size, orientation) {
+    this.ships_orien[0] = orientation;
+    this.ships_size[0] = size;
+    const cells = document.querySelectorAll("#playerfield .cell");
+  
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        const cellIndex = i * 10 + j;
+  
+        if (this.placeAblePos[cellIndex] < 2) {
+          if (orientation === 'v') {
+            if (i > 10 - size) {
+              cells[cellIndex].classList.add("usable");
+              cells[cellIndex].classList.remove("water");
+              this.placeAblePos[cellIndex] = 1; 
+            } else {
+              cells[cellIndex].classList.add("water");
+              cells[cellIndex].classList.remove("usable");
+              this.placeAblePos[cellIndex] = 0; 
+            }
+          } else if (orientation === 'h') {
+            if (j > 10 - size) {
+              cells[cellIndex].classList.add("usable");
+              cells[cellIndex].classList.remove("water");
+              this.placeAblePos[cellIndex] = 1; 
+            } else {
+              cells[cellIndex].classList.add("water");
+              cells[cellIndex].classList.remove("usable");
+              this.placeAblePos[cellIndex] = 0; 
+            }
+          }
+        }
+      }
+    }
+    for(let i = 0; i < 10; i++){
+      for(let j = 0; j < 10; j++){
+        let at = (i*10)+j 
+        if(this.placeAblePos[at] >= 2){
+           if(orientation === 'h'){
+            at--
+            for(let q = 1, k = j; q < size && k >= 0; q++, k--){
+              if(this.placeAblePos[at] < 2){
+                cells[at].classList.add("usable");
+                cells[at].classList.remove("water");
+                this.placeAblePos[at] = 1; 
+                if(at-1 >= 0)
+                  at--
+              }
+            }
+           }
+           else if(orientation === 'v'){
+            at = at-10
+            for(let q = 1, k = i; q < size && k >= 0; k--, q++){
+              if(this.placeAblePos[at] < 2){
+                cells[at].classList.add("usable"); 
+                cells[at].classList.remove("water");
+                this.placeAblePos[at] = 1; 
+                if(at-10 >= 0)
+                  at = at-10
+                  else
+                    break          
+              }
+            }
+           }           
+        }
+      }
+    }
+  },
+  
+ update(){
+  const cells = document.querySelectorAll("#playerfield .cell");
+  for(let i = 0; i < 100; i ++){
+    const p = this.placeAblePos[i] ;
+    if(p !== 2 && p !== 3 && p !== 4 && p !== 5 &&(p === 1 || p === 0)){  
+      this.updateCell(cells[i],i);
+      }      
   }
 
+  for(let i = 0; i <= this.Ships.length; i++){ 
+    if (this.Ships[i] && this.Ships[i].classList && this.Ships[i+1] && this.Ships[i+1].classList) {
+    if(this.Ships_Size[i] == 5) {
+      if(this.shipsNum[0] == 0) {
+        this.Ships[i].classList.add("usable");this.Ships[i+1].classList.add("usable");}
+      else{
+         this.Ships[i].classList.remove("usable"); this.Ships[i+1].classList.remove("usable");}
+    }
+    else if(this.Ships_Size[i] == 4) {
+      if(this.shipsNum[1] == 0) {
+         this.Ships[i].classList.add("usable");  this.Ships[i+1].classList.add("usable");}
+      else {
+        this.Ships[i].classList.remove("usable"); this.Ships[i+1].classList.remove("usable");}
+    }
+    else if(this.Ships_Size[i] == 3) {
+      if(this.shipsNum[2] == 0) {
+        this.Ships[i].classList.add("usable");  this.Ships[i+1].classList.add("usable");}
+      else {
+        this.Ships[i].classList.remove("usable");  this.Ships[i+1].classList.remove("usable");}
+    }
+    else if(this.Ships_Size[i] == 2) {
+      if(this.shipsNum[3] == 0) {
+        this.Ships[i].classList.add("usable");  this.Ships[i+1].classList.add("usable");}
+      else {
+        this.Ships[i].classList.remove("usable"); this.Ships[i+1].classList.remove("usable");}
+    }
+  }
+}
+  
+},
+updateCell(cell,i){
+  cell.classList.add("water");      
+  cell.classList.remove("ship");
+  cell.classList.remove("topShip");
+  cell.classList.remove("bottomShip");
+  cell.classList.remove("leftShip");
+  cell.classList.remove("rightShip");
+  cell.classList.remove("usable"); 
+  this.placeAblePos[i] = -1;     
+},
+readyToplay(){ 
+  this.update();
+  const cells = document.querySelectorAll("#playerfield .cell");
+  for(let i = 0; i < 100; i ++){
+    const p = this.placeAblePos[i] ;
+    if(p === 6){    
+      cells[i].classList.add("water");      
+      cells[i].classList.remove("usable");  
+      this.placeAblePos[i] = -1;     
+      }      
+  }
+},
+autoPlacing(player){      
+  this.update();
+  let ships = [5,4,4,3,3,3,2,2,2,2];
+  let ori = ['v','h'];
+  let i = 0;
+  while(i < 10){ 
+    this.ships_size[0] = ships[i]
+    var o = Math.floor(Math.random() * 2);
+    this.showPositions(this.ships_size[0], ori[o]);
+    var j = Math.floor(Math.random() * 100);
+    if(this.shipPlacer(j,player)){
+      i++;
+    } 
+  }
+ 
+},
+updateClickablity(Cells){ 
+  Cells.forEach(cell => {
+    cell.removeEventListener("click", cell.clickHandler);
+  });
+},
+shipHit(at, filed, turn){  
+  if (filed[at].classList.contains("water")) {
+    filed[at].classList.remove("water");
+  } 
+  if(turn === 2){
+    if(this.placeAblePos[at] >= 2 && this.placeAblePos[at] <= 5){
+      filed[at].classList.add("hitShip");
+      this.placeAblePos[at] = -5;
+      return true;
+    }else { 
+      filed[at].classList.add("hitWater");
+      return false;
+    }
+  }else{ 
+    if(this.placeAblePosCom[at] >= 2 && this.placeAblePosCom[at] <= 5){      
+      filed[at].classList.add("hitShip");
+      this.placeAblePosCom[at] = -10;
+      return true;
+    }    
+    else{       
+      filed[at].classList.add("hitWater");
+      this.placeAblePosCom[at] = 1; 
+      return false;   
+    } 
+  }
+},
+winerChecker(turn){ 
+  for(let i = 0; i < 100; i++){ 
+    if(turn===2){
+      if(this.placeAblePos[i] >= 2 && this.placeAblePos[i] <= 5) 
+      {  return false; }
+    }
+    else{
+      if(this.placeAblePos[i] >= 2 && this.placeAblePos[i] <= 5)
+      {  return false;}
+    }
+  }
+  if(turn === 2) this.winer = "Player";
+  if(turn === 1) this.winer = "Computer"; 
+  this.GameFinshed = true;  
+    return true;
+},
+isContain(grid, that){
+  for(let i = 0; i < grid.length; i++){
+    
+}
+},
+ 
+startPlaying(at) { 
+  if(this.winerChecker(this.turn)) 
+  {this.showMessage((this.winer+" won the Game"), "green"); return;}
+  const fieldCom = document.querySelectorAll("#computerfield .cell");
+  const fieldPly = document.querySelectorAll("#playerfield .cell");
+  if (this.turn === 1) {
+    if (!this.shipHit(at, fieldCom, this.turn)) {
+      this.turn = 2;
+      this.showMessage("It's Server's turn. I hit the water.", "red");
+    }
+  }
+
+  if (this.turn === 2) {
+    let still = true;
+
+    const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+    (async () => {
+      while (still) {
+        await delay(2000);
+        const randomAt = Math.floor(Math.random() * 100);
+
+        if (!this.shipHit(randomAt, fieldPly, 2)) {
+          still = false;
+          this.turn = 1;
+          this.showMessage("It's Player's turn. I hit the water.", "red");
+        } else {
+          this.showMessage("It's Server's turn. I hit the Ship.", "green");
+        }
+      }
+    })();
+  }
+},
+
+/*remoteLogic() {
+  url: "https://www2.hs-esslingen.de/~melcher/internet-technologien/sinkship/",
+  token: 0,
+  responseX: "",
+  async init() {
+    const request = `?request=start&userid=jasait02`;
+    const response = await this.fetchAndDecode(request);
+    this.token = response.resp.token;
+    document.getElementById("infoTextField").textContent =
+      response.resp.statusText;
+  },
+  async fetchAndDecode(request) {
+    const resp = await fetch(`${this.url}` + `${request}`).then((response) =>
+      response.json()
+    );
+
+    return { resp };
+  },
+},*/
 
 };
